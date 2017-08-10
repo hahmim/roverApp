ab76ad73617aac9f8c326e9e2bebd4d025e1339d
hahmim <hahmim@gmail.com>
2017-08-09 23:21:26 -0400

    added a bean for light weight

--- src/main/java/com/hakim/marsexp/mars_expedition/App.java
+++ src/main/java/com/hakim/marsexp/mars_expedition/App.java
@@ -2,6 +2,10 @@ package com.hakim.marsexp.mars_expedition;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.hakim.marsexp.exception.InvalidInputException;
 import com.hakim.marsexp.helper.AppHelper;
 import com.hakim.marsexp.helper.AppHelperImpl;
@@ -15,10 +19,13 @@ import com.hakim.marsexp.util.ExpeditionConstants;
  */
 public class App 
 {
-	
+
 
 	public static void main(String[] args) {
-		AppHelper appHelper = new AppHelperImpl();
+		
+    	ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+    	AppBean AppBean = (AppBean) ctx.getBean("AppBean");
+		
 		ArrayList<Character> cardinals = new ArrayList<Character>();
 		ArrayList<Character> validationList = new ArrayList<Character>();
 		cardinals.add(ExpeditionConstants.North);
@@ -28,13 +35,14 @@ public class App
 		validationList.add(ExpeditionConstants.Left);
 		validationList.add(ExpeditionConstants.Right);
 		validationList.add(ExpeditionConstants.Move);
-		try {
-			long timeNow1 = System.currentTimeMillis();
-			appHelper.moveRover(cardinals, validationList);
-			long timeNow2 = System.currentTimeMillis();
-			System.out.println("the time consumed was " + (timeNow2 - timeNow1) + " miliseconds");
-		} catch (InvalidInputException e) {
-			e.printStackTrace();
-		}
+		long timeNow1 = System.currentTimeMillis();
+			try {
+				AppBean.callMoveRover(cardinals, validationList);
+			} catch (InvalidInputException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		long timeNow2 = System.currentTimeMillis();
+		System.out.println("the time consumed was " + (timeNow2 - timeNow1) + " miliseconds");
 	}
 }
